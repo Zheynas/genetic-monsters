c8083effd7e215886e9eeaf89600932f3b3cba08
Ska101 <samantha.hancock@sky.uk>
2016-12-09 11:46:27 +0000

    breeding.js done
    *tests pending
    *Do we want to split up the stat change stuff?

--- breeding.js
+++ breeding.js
@@ -1,50 +1,52 @@
 var utility = require('./utility.js');
+var selection = require('./selection.js');
+
+var exports = module.exports = {};
 
 // Selects parents by the roulette selection method.
-createChildren = function(){
-  createRouletteSelectionArray();
+exports.createChildren = function(){
+  selection.createRouletteSelectionArray(population);
 
   for(x=0;x<numberOfChildrenCreated; x++)
   {
-    parent1 = population[rouletteChooser()-1];
-    parent2 = population[rouletteChooser()-1];
+    parent1 = population[selection.rouletteChooser()-1];
+    parent2 = population[selection.rouletteChooser()-1];
 
     // Breeds the two parents to create a child
-    crossoverBreeding(parent1,parent2);
+    exports.crossoverBreeding(parent1,parent2);
   }
-
 };
 
 // Takes two parents and creates a child using crossover breeding.
-crossoverBreeding = function(parent1,parent2){
+exports.crossoverBreeding = function(parent1,parent2){
 
   var childMonster = new Object();
 
-  childHealth = breedStat(
+  childHealth = exports.breedStat(
     parent1.health_value, parent1.health_code,
     parent2.health_value, parent2.health_code);
 
   childMonster.health_value = childHealth[0];
   childMonster.health_code = childHealth[1];
 
-  childSpeed = breedStat(
+  childSpeed = exports.breedStat(
     parent1.speed_value, parent1.speed_code,
     parent2.speed_value, parent2.speed_code);
 
   childMonster.speed_value = childSpeed[0];
   childMonster.speed_code = childSpeed[1];
 
-  childDamage = breedStat(
+  childDamage = exports.breedStat(
     parent1.damage_value, parent1.damage_code,
     parent2.damage_value, parent2.damage_code);
 
   childMonster.damage_value = childDamage[0];
   childMonster.damage_code = childDamage[1];
 
-  childMonster.type_code = breedTypeCode(parent1.type_code,parent2.type_code);
+  childMonster.type_code = exports.breedTypeCode(parent1.type_code,parent2.type_code);
 
   //Checks if child will mutate
-  if(mutateChild(childMonster) == false)
+  if(exports.mutateChild(childMonster) == false)
   {
     childMonster.fitness = fitnessFunction(childMonster);
     population.push(childMonster);
@@ -53,7 +55,7 @@ crossoverBreeding = function(parent1,parent2){
 };
 
 // Chances the codes at random to simulate mutation
-mutateChild = function(childMonster){
+exports.mutateChild = function(childMonster){
 
   randomNumber = utility.getRandom(0,99);
   if(randomNumber< mutantChance)
@@ -86,3 +88,95 @@ mutateChild = function(childMonster){
     return false;
   }
 };
+
+exports.breedTypeCode = function(parent1,parent2){
+
+  parentOrder = utility.getRandom(0,2); // 0 or 1
+  randomNumber = utility.getRandom(0,parent1.length);
+  splitPoint = parent1.length - randomNumber;
+
+  //Parent 1 on the left of the split
+  if(parentOrder == 1){
+    parent1Section = parent1.substring(0,splitPoint);
+    parent2Section = parent2.substring(splitPoint)
+    childType = parent1Section.concat(parent2Section);
+  }
+  // Parent 2 on the left of the split
+  else{
+    parent2Section = parent2.substring(0,splitPoint);
+    parent1Section = parent1.substring(splitPoint)
+    childType = parent2Section.concat(parent1Section);
+  }
+
+  // make object to add dominantType at the same time
+  return childType;
+
+}
+
+
+exports.breedStat = function(parent1Value, parent1Code, parent2Value, parent2Code){
+
+  parentOrder = utility.getRandom(0,2); // 0 or 1
+  randomNumber = utility.getRandom(0,4); // 0 - 3  ** this is to do with the length of the binary string
+  splitPoint = parent1Code.length - randomNumber;
+
+  //Parent 1 on the left of the split
+  if(parentOrder == 1){
+    parent1Section = parent1Code.substring(0,splitPoint);
+    parent2Section = parent2Code.substring(splitPoint)
+    childMonsterCode = parent1Section.concat(parent2Section);
+  }
+  // Parent 2 on the left of the split
+  else{
+    parent2Section = parent2Code.substring(0,splitPoint);
+    parent1Section = parent1Code.substring(splitPoint)
+    childMonsterCode = parent2Section.concat(parent1Section);
+  }
+
+  // turns binary into decimal
+  childMonsterValue = parseInt(childMonsterCode,2);
+
+  if(childMonsterValue == 0)
+  {
+    childMonsterValue =1;
+    childMonsterCode = utility.decbin(childMonsterValue,8)
+  }
+
+  return[childMonsterValue, childMonsterCode]
+};
+
+
+
+// decideDominantType = function(typeCode){
+//
+//   dominantType="";
+//   aCount = 0;
+//   bCount = 0;
+//   cCount = 0;
+//
+//   for(i=0;i<lengthOfTypeCode; i++)
+//   {
+//     switch(typeCode.charAt(i)){
+//
+//       case("A"):
+//       aCount = aCount +1;
+//       break;
+//
+//       case("B"):
+//       bCount = bCount +1;
+//       break;
+//
+//       case("C"):
+//       cCount = cCount +1;
+//       break;
+//     }
+//   }
+//
+//   if(aCount>)
+//
+//
+//
+//
+//
+//   return dominantType;
+// }
---
+++ tests/breeding.test.js
@@ -0,0 +1,36 @@
+var chai = require('chai');
+var breeding = require('../breeding.js');
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+var monster = {
+  health_value: 3,
+  health_code:"00000011",
+  speed_value: 3,
+  speed_code:"00000011",
+  damage_value: 3,
+  damage_code:"00000011",
+  type_code: "AAAAA",
+  dominant_type: "A",
+  fitness : 15
+}
+
+var monster2 = {
+  health_value: 1,
+  health_code:"00000001",
+  speed_value: 1,
+  speed_code:"00000001",
+  damage_value: 1,
+  damage_code:"00000001",
+  ttype_code: "CCCCC",
+  dominant_type: "C",
+  fitness : 15
+}
+
+describe('Breeding functions', function(){
+
+  it('Child monster has all stats');
+  it('Child has a type');
+  it('Correct number of childrent created');
+})
